f5bb8ff3e6017d1c588a8983eec792157e5fcf64
natherreran <natalia.h@rompecabeza.cl>
2018-06-27 17:50:12 -0400

    focus set

--- js/validar-acordeon.js
+++ js/validar-acordeon.js
@@ -16,6 +16,9 @@ function validateAccordion() {
     if (v1 && v2 && v3 && v4 && v5 && v6 && v7 && v8 && v9 && v10 && v11 && v12 && v13) {
         return true
     } else {
+        $(".rut-error").first().focus();
+        // var scroll = $(window).scrollTop();
+        // window.scrollTo(0,scroll-83)
         return false;
     }
 };
